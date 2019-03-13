8b3b35de78fd3838fab5985515eff1b7bb12e3d3
hsonne <hauke.sonnenberg@gmx.de>
2019-03-13 09:28:48 +0100

    Bring functions into alphabetical order

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
-# get_col ----------------------------------------------------------------------
-get_col <- kwb.utils::selectColumns
-
-# get_ele ----------------------------------------------------------------------
-get_ele <- kwb.utils::selectElements
+# cat_green_bold_0 -------------------------------------------------------------
+cat_green_bold_0 <- function(...)
+{
+  cat(crayon::green(crayon::bold(paste0(...))))
+}
 
 # delete_empty_columns_right ---------------------------------------------------
 delete_empty_columns_right <- function(x)
@@ -14,14 +14,14 @@ delete_empty_columns_right <- function(x)
   x[, seq_len(last_column_index), drop = FALSE]
 }
 
+# get_col ----------------------------------------------------------------------
+get_col <- kwb.utils::selectColumns
+
+# get_ele ----------------------------------------------------------------------
+get_ele <- kwb.utils::selectElements
+
 # stop_formatted ---------------------------------------------------------------
 stop_formatted <- function(fmt, ...)
 {
   stop(sprintf(fmt, ...), call. = FALSE)
-}
-
-# cat_green_bold_0 -------------------------------------------------------------
-cat_green_bold_0 <- function(...)
-{
-  cat(crayon::green(crayon::bold(paste0(...))))
 }
