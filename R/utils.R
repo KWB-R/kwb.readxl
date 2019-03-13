cf3a03d0a5fb0a65936c996a0ad9414ee8c2a34d
hsonne <hauke.sonnenberg@gmx.de>
2019-03-13 09:43:01 +0100

    Move print_logical_matrix() to utils.R and improve
    Add arguments do_print, chars

--- R/utils.R
+++ R/utils.R
@@ -20,6 +20,38 @@ get_col <- kwb.utils::selectColumns
 # get_ele ----------------------------------------------------------------------
 get_ele <- kwb.utils::selectElements
 
+# print_logical_matrix ---------------------------------------------------------
+print_logical_matrix <- function(
+  x, invert = FALSE, do_print = TRUE, chars = c("x", " ")
+)
+{
+  stopifnot(is.matrix(x))
+  
+  if (! is.logical(x)) {
+    x <- as.logical(x)
+  }
+
+  if (invert) {
+    x <- ! x
+  }
+  
+  y <- matrix(chars[2], nrow = nrow(x), ncol = ncol(x))
+  
+  y[x] <- chars[1]
+  
+  result <- kwb.utils::pasteColumns(as.data.frame(y), sep = "|")
+  
+  if (do_print) {
+    
+    writeLines(result)
+    invisible(result)
+    
+  } else {
+    
+    result
+  }
+}
+
 # stop_formatted ---------------------------------------------------------------
 stop_formatted <- function(fmt, ...)
 {
